ae4013563b52e85e24a795425a0ef6590045073c
howther <atsukiexfeel@yahoo.co.jp>
2017-09-14 09:21:22 +0900

    バックアップ

---
+++ mojiokoshiSplit.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import sys
+import os
+import argparse
+
+class Audio:
+    def __init__(self, filename):
+        self.filename = filename
+        self.nframe = 0 #sample数
+        self.rframe = 1 #sample rate数
+        self.channels = 0 #チャンネル
+        self.sampwidth = 0 #sample size
+
+    #return audio file as array of integer
+    def read(self):
+        #read wav file
+        wav = wave.open(self.filename, "r")
+
+        #move to head of the audio file
+        wav.rewind()
+
+        self.nframe = wav.getnframes()
+        self.rframe = wav.getframerate()
+        self.channels = wav.getnchannels()
+        self.sampwidth = wav.getsampwidth()
+
+        # read to buffer as binary format
+        buf = wav.readframes(self.nframe)
+        wav.close()
+
+        if(self.channels == 1):
+            audio_data = np.frombuffer(buf, dtype="int16")
+        elif(self.channels == 2):
+            audio_data = np.frombuffer(buf, dtype="int32")
+
+        return audio_data
+
+    def is_split(self, y, size):
+        if (len(y) > size):
+            return True
+        return False
+
+    def is_silence(self, iterable):
+        if max(iterable) < 4096:
+            return True
+        else:
+            return False
+
+    def split_equal(self, y, split_num):
+        segment_points = list()
+        size = len(y) / split_num;
+        count = 1
+
+        while True:
+            if(count==(split_num)):
+                break
+            segment_points.append(size*count)
+            count = count + 1
+
+        segment_points.append(len(y))
+        return segment_points
+
+    def split(self, y, time_term, slience_time_term):
+        size = int(self.rframe * time_term) #分割サイズ
+        silence_size = int(self.rframe * slience_time_term) #0.1s 無音の場合切る
+
+        segment_points = list()
+
+        count = 1
+        start = 0
+        end = 0
+        offset = 0 #segmentポイントから時間を図り始める
+
+        print('総フレーム数：' + str(self.nframe))
+        print('総時間：' + str(int(self.nframe / size)) + '秒')
+
+        while True:
+            end = end + 441
+            # end = start + int(count_silence + 1) * silence_size
+
+            if not self.is_split(y[start:self.nframe], size):
+                break
+
+            if end % size == 0:
+                second = end / size
+                print(str(second) + '秒まで処理')
+
+            z1 = np.absolute(y[start:end])
+            check_silence = int(end + silence_size)
+            z2 = np.absolute(y[end:check_silence])
+
+            if self.is_split(y[end:self.nframe], size):
+                if self.is_silence(z2):
+                    segment_points.append(end + int(silence_size / 2))
+                    count = count + 1
+
+                    offset = end + int(silence_size / 2)
+                    start = int(offset)
+                    end = int(offset) + size
+            else:
+                break
+
+        segment_points.append(len(y))
+
+        return segment_points
+
+    #audioの時間、単位: s
+    def audio_time(self):
+        return float(self.nframe/self.rframe)
+
+    def write(self, filename, segment_points, audio_data):
+        count = 0
+        start = 0
+        for end in segment_points:
+            version = "{:05d}".format(count)
+            w = wave.Wave_write(filename+"/"+ filename+"-"+version+".wav")
+            w.setnchannels(self.channels)
+            w.setsampwidth(self.sampwidth)
+            w.setframerate(self.rframe)
+            w.writeframes(audio_data[int(start):int(end)])
+            start = end
+            w.close()
+            count = count+1
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='WAV ファイル分割プログラム')
+    parser.add_argument('-i', action='store', dest='file_name',
+                help='wavファイルを指定', required=True, type=str)
+
+    parser.add_argument('-type', action='store', type=str, dest='type', default="equal",
+                help='分割種類の選択: 等分割、無音分割(equal | optional), デフォルト: euqal')
+    parser.add_argument('-n', action='store', type=int, dest='split_num', default=2,
+                help='等分割の場合, 分割数を設定, デフィルト: 2')
+
+    parser.add_argument('-t', action='store', type=float, dest='time', default=300,
+                help='各分割ファイルの最低サイズ, 単位: 秒, デフォルト: 300s')
+    parser.add_argument('-st', action='store', type=float, dest='slience_time', default=1,
+                help='無音ターム, 単位: 秒, デフォルト: 1s')
+
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+    results = parser.parse_args()
+    return results
+
+try:
+    #引数の解析
+    args = parse_args()
+
+    #add progress bar, time_term, 無音term
+    audio = Audio(args.file_name)
+    audio_data  = audio.read()
+    # time = audio.audio_time()
+    if args.type=="equal":
+        segment_points = audio.split_equal(audio_data, args.split_num)
+    elif args.type=="optional":
+        segment_points = audio.split(audio_data, args.time, args.slience_time)
+
+    output = os.path.splitext(os.path.basename(args.file_name))
+    # os.system("sh check.sh " + output[0])
+    audio.write(output[0], segment_points, audio_data)
+except:
+    print ("FALSE")
+    raise
---
+++ mojiokoshiWrite.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: UTF-8
+import requests
+import os
+import time
+import argparse
+
+GOOGLE_APIKEY = 'AIzaSyAI2lg0FPozbJCPp8qGumAObRMJw58m5aw'
+VOICE_REC_PATH = ''
+FILE_NUM = 1
+
+def recognize(file_number):
+    file_num_str = ''
+    if file_number < 10:
+        file_num_str = '-0000' + str(file_number)
+    elif file_number < 100:
+        file_num_str = '-000' + str(file_number)
+    elif file_number < 1000:
+        file_num_str = '-00' + str(file_number)
+    elif file_number < 10000:
+        file_num_str = '-0' + str(file_number)
+    else:
+        file_num_str = '-' + str(file_number)
+
+    voice_file = VOICE_REC_PATH + file_num_str + '.wav'
+    print(voice_file + ' recognizing...')
+    f = open(voice_file, 'rb')
+    voice = f.read()
+    f.close()
+
+    url = 'https://www.google.com/speech-api/v2/recognize?xjerr=1&client=chromium&'\
+        'lang=ja-JP&maxresults=10&pfilter=0&xjerr=1&key=' + GOOGLE_APIKEY  
+    hds = {'Content-type': 'audio/l16; rate=44100'}
+    
+    try:    
+        reply = requests.post(url, data=voice, headers=hds).text
+    except IOError:
+        return '#CONN_ERR'
+    except:
+        return '#ERROR'
+        
+    print('results:', reply)
+    
+    objs = reply.split(os.linesep)
+    for obj in objs:
+        if not obj:
+            continue
+
+        obj = obj.replace('{"result":[]}\n{"result":[{"alternative":[{"transcript":"', '')
+        index = obj.find('","confidence"')  # indexは1(2文字目)
+        obj = obj[0:index]
+        index = obj.find('"},{"transcript"')
+        obj = obj[0:index]
+
+        if len(obj) == 0:
+            continue
+        return obj
+    return ""
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='WAV ファイル分割プログラム')
+    parser.add_argument('-i', action='store', dest='file_name',
+                help='wavファイルを指定', required=True, type=str)
+
+    parser.add_argument('-n', action='store', type=int, dest='split_num', default=1,
+                help='ファイル数を設定, デフィルト: 1')
+
+    results = parser.parse_args()
+    return results
+
+def current_milli_time():
+    return int(round(time.time() * 1000))
+
+if __name__ == '__main__':
+    args = parse_args()
+    VOICE_REC_PATH = './' + args.file_name + '/' + args.file_name
+    FILE_NUM = args.split_num
+
+    for file_number in range(FILE_NUM):
+        t0 = current_milli_time()
+        message = recognize(file_number)
+        print('recognized:' + str(current_milli_time() - t0) + 'ms')
+        if message == '#CONN_ERR':
+            print('internet not available')
+            message = ''
+        elif message == '#ERROR':
+            print('voice recognizing failed')
+            message = ''
+        else:
+            if not message == '{"result":[]}':
+                if message.find('<!DOCTYPE html>') != -1:
+                    message = message + '\n'
+                message = '※サーバーエラー：' + str(file_number)
+                print('your words:' + message)
+            else:
+                message = '\n'
+                print('your words: no message')
+
+        # sample.txtが存在しない場合は、自動的に新規作成される。
+        writer = open('answer.txt', 'a')
+
+        # テキストファイルへの書き込み
+        writer.write(message)
+
+        # 移動中のデータをクリア
+        writer.flush()
+
+        # テキストファイルとの接続を切る
+        writer.close()
